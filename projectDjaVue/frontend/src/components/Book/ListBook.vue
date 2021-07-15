f7a139b12deebd6584cebce4a70136674008a290
Heredu <hernaromero@frba.utn.edu.ar>
2021-07-14 22:48:06 -0300

    frontend created ini

---
+++ projectDjaVue/frontend/src/components/Book/ListBook.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>List Books</h2>
+            </div>
+        </div>
+    </div>       
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
